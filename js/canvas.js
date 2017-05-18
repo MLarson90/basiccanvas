e791dac66871a41d0ff50bfc3bef721b0d1d2613
Max Larson <mr.m.larson90@gmail.com>
2017-05-18 15:06:35 -0700

    make bubbles

--- js/canvas.js
+++ js/canvas.js
@@ -5,7 +5,21 @@ canvas.height = window.innerHeight;
 var c = document.getElementById("myCanvas"),
     ctx = c.getContext("2d");
 
+    var mouse = {
+      x:undefined,
+      y:undefined
+    }
+    maxRadius = 50;
 
+window.addEventListener('click', function(event){
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+window.addEventListener('resize', function(event){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  init();
+})
 
 function Circle(x, y, dx, dy, radius){
   this.x = x;
@@ -17,6 +31,13 @@ function Circle(x, y, dx, dy, radius){
     ctx.beginPath();
     ctx.arc(this.x,this.y,this.radius, 0 ,Math.PI*2);
     ctx.strokeStyle = "blue";
+    ctx.fillStyle="lightblue";
+    ctx.stroke();
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(this.x+3,this.y-4,this.radius-6, 0 ,Math.PI*2);
+    ctx.fillStyle="white";
+    ctx.strokeStyle = "lightblue";
     ctx.stroke();
     ctx.fill();
   }
@@ -27,22 +48,44 @@ function Circle(x, y, dx, dy, radius){
     if( this.y + this.radius > innerHeight || this.y - this.radius < 0){
       this.dy = -this.dy;
     }
+    if(mouse.x > this.x - this.radius && mouse.x < this.x +this.radius &&  mouse.y > this.y - this.radius && mouse.y < this.y  + this.radius){
+      if(this.radius< maxRadius){
+      this.radius ++;
+      }
+      this.dx = this.dx + .5;
+      this.dy = this.dy + .5;
+      morecircles();
+    }
     this.x += this.dx;
     this.y += this.dy;
     this.draw();
+
   }
 }
-
 var circlesArray= [];
-for(var i=0; i<100; i++){
-  var radius = 30;
+function init(){
+morecircles = function(){
+for(var i=0; i<2; i++){
+  if(circlesArray.length<800){
+  var radius =(Math.random() + .9) * 9;
   var x = Math.random() * (innerWidth -radius *2) + radius;
   var y = Math.random() * (innerHeight -radius *2) + radius;
   var dx = (Math.random() - 0.5) * 8;
   var dy = (Math.random() - 0.5) * 8;
   circlesArray.push(new Circle(x,y,dx,dy,radius));
 }
-
+}
+}
+for(var i=0; i<20; i++){
+  var radius =(Math.random() + .9) * 9;
+  var x = Math.random() * (innerWidth -radius *2) + radius;
+  var y = Math.random() * (innerHeight -radius *2) + radius;
+  var dx = (Math.random() - 0.5) * 8;
+  var dy = (Math.random() - 0.5) * 8;
+  circlesArray.push(new Circle(x,y,dx,dy,radius));
+}
+}
+init();
 function animate(){
   requestAnimationFrame(animate);
   ctx.clearRect(0,0,innerWidth,innerHeight);
